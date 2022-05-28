3cb8583177ca352a9854115b2952557bd6d03772
concave-dash <kwerkee12@gmail.com>
2022-05-28 14:17:03 -0400

    map db

---
+++ db.js
@@ -0,0 +1,51 @@
+// const DB = require('./db.js');
+// const db = new DB(filename);
+// db.set(1, 2)
+// for [k,v] of db
+const main = require('require-main-filename')();
+
+// TODO: extends Map
+module.exports = class DB {
+    #path;
+    #map;
+    constructor(filename) {
+        this.#path = `${main}/${filename}.db`;
+        this.#map = this.load();
+    }
+    set(k, v) {
+        const r = this.#map.set(k, v);
+        this.save();
+        return r;
+    }
+    get(k) {
+        const r = this.#map.get(k);
+        return r;
+    }
+    [Symbol.iterator]() {
+        const r = this.#map[Symbol.iterator]();
+        return r;
+    }
+    clear() {
+        const r = this.#map.clear();
+        fs.unlinkSync(this.#path);
+        return r;
+    }
+    get size() {
+        const r = this.#map.size;
+        return r;
+    }
+    load() {
+        let r;
+        try {
+            const data = fs.readFileSync(this.#path);
+            r = new Map(JSON.parse(data))
+        } catch (err) {
+            r = new Map();
+        }
+        return r;
+    }
+    save() {
+        const data = JSON.stringify([...this.#map]);
+        fs.writeFileSync(this.#path, data);
+    }
+}
