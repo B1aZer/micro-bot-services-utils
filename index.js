084784abc2542b3c1d6481a14bcb5e7981a9a7b0
concave-dash <kwerkee12@gmail.com>
2022-05-30 00:06:15 -0400

    random num

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ module.exports = {
         const randomEl = module.exports.pickRandom(array, rnd);
         const indexOfRemEl = array.indexOf(randomEl);
         const removedElArr = array.splice(indexOfRemEl, indexOfRemEl + 1);
-        return removedElArr[0];
+        return removedElArr[0] ?? '';
+    },
+    randomIntFromInterval(min, max, rnd) { // min and max included 
+        return rnd * (max - min + 1) + min;
     }
 }
